eefd3bd091188857d0202914b88afa6ffe4f5a78
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-10-11 00:05:53 +0300

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args){
+        String year = "2023";
+        String name = "Artem";
+        System.out.printf("Hello, my name is %s.\nI was created in %s%n.", name, year);
+    }
+}
