25405b274f5ced1994ef15b96550936e6fc3719a
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-10-11 00:20:15 +0300

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,13 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
         String year = "2023";
-        String name = "Artem";
-        System.out.printf("Hello, my name is %s.\nI was created in %s%n.", name, year);
+        String botName = "Artem";
+        System.out.printf("Hello, my name is %s.\nI was created in %s.\nPlease, remind me your name.\n", botName, year);
+        String userName = in.nextLine();
+        System.out.printf("What a great name you have, %s!", userName);
     }
 }
