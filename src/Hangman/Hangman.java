0cde48cd68c495e956e7058b54b3a013f3dd75b8
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-10-25 23:51:14 +0300

    Hangman 3-rd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,14 +1,17 @@
 package Hangman;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
     public static void main(String[] args){
+        String[] words_list = {"java", "kotlin", "python", "javascript"};
         System.out.println("HANGMAN");
-        hangman("java");
+        hangman(words_list);
     }
 
-    private static void hangman(String answer){
+    private static void hangman(String[] words_list){
         System.out.print("Guess the world: ");
+        String answer = choice(words_list);
         String player_input = input();
         if (player_input.equals(answer)){
             System.out.println("You survived!");
@@ -23,4 +26,8 @@ public class Hangman {
         return in.nextLine();
     }
 
+    private static String choice(String[] words){
+        Random rnd = new Random();
+        return words[rnd.nextInt(words.length)];
+    }
 }
