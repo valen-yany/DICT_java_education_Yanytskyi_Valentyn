86e774cd3bd22897b43084517c2aa2aed3220943
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-10-25 23:40:49 +0300

    Hangman 2-nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,11 +1,26 @@
 package Hangman;
+import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args){
-        announcer();
+        System.out.println("HANGMAN");
+        hangman("java");
     }
 
-    private static void announcer(){
-        System.out.println("HANGMAN\nThe game will be available soon.");
+    private static void hangman(String answer){
+        System.out.print("Guess the world: ");
+        String player_input = input();
+        if (player_input.equals(answer)){
+            System.out.println("You survived!");
+        }
+        else{
+            System.out.println("You lost!");
+        }
     }
+
+    private static String input(){
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
 }
