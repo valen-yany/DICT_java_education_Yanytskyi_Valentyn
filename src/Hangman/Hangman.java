e19802673e35f404e4998b1688f4eecb1201055a
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-10-25 23:21:58 +0300

    Hangman 1-st stage

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,11 @@
+package Hangman;
+
+public class Hangman {
+    public static void main(String[] args){
+        announcer();
+    }
+
+    private static void announcer(){
+        System.out.println("HANGMAN\nThe game will be available soon.");
+    }
+}
