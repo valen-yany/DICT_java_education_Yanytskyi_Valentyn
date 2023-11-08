72a7b771c8a4e58fbc05fb28695f9f119662b66b
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-11-08 18:06:25 +0200

    CoffeeMachine 2-nd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,13 +1,15 @@
 package CoffeeMachine;
+import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args){
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = in.nextInt();
+        getIngredients(cups);
+    }
+
+    public static void getIngredients(int cups){
+        System.out.printf("For %d cups of coffee you will need:\n%d ml of water\n%d ml of milk\n%d g of coffee beans", cups, cups * 200, cups * 50, cups*15);
     }
 }
