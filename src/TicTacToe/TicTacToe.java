d7661bd0c7e721ab7bea1fb2b60d6244438aa5fc
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-11-23 00:56:22 +0200

    TicTacToe 4-th stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -5,22 +5,10 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args){
         Board b = new Board();
+        Player p = new Player('X');
         b.PrintGrid();
-        if(b.CheckImpossible()){
-            System.out.println("Impossible");
-        }
-        else if(b.CheckWin('X')){
-            System.out.println("X wins");
-        }
-        else if(b.CheckWin('O')){
-            System.out.println("O wins");
-        }
-        else if(b.CheckEmpty()){
-            System.out.println("Game not finished");
-        }
-        else{
-            System.out.println("Draw");
-        }
+        p.MakeMove(b);
+        b.PrintGrid();
     }
 
 
@@ -86,5 +74,55 @@ class Board{
             }
         }
         return false;
+    }
+
+    public void FillCell(int[] coords, char sym){
+        grid[coords[0]][coords[1]] = sym;
+    }
+
+    public boolean isCellOccupied(int[] coords){
+        return!(grid[coords[0]][coords[1]] == '_');
+    }
+}
+
+class Player{
+    char player_sym;
+    public Player(char player_sym){
+        this.player_sym = player_sym;
+    }
+
+    public char GetSymbol(){
+        return player_sym;
+    }
+
+    public void MakeMove(Board board){
+        Scanner in = new Scanner(System.in);
+        outer:
+        do {
+            String[] coords;
+            System.out.print("Enter cells: ");
+            coords = in.nextLine().trim().split("\\s+");
+            if (coords.length != 2){
+                continue;
+            }
+            int[] num_coords = new int[2];
+            for (int i = 0; i < 2; i++) {
+                if(!coords[i].matches("[0-9]")){
+                    System.out.println("You should enter numbers!");
+                    continue outer;
+                }
+                num_coords[i] = Integer.parseInt(coords[i]) - 1;
+                if (num_coords[i] < 0 || num_coords[i] > 2){
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    continue outer;
+                }
+            }
+            if(board.isCellOccupied(num_coords)){
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+            board.FillCell(num_coords, player_sym);
+            break;
+        }while(true);
     }
 }
