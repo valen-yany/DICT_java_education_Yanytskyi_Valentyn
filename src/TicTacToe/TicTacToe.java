a98614bd606540dd38606b977034122f9fa63478
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-11-22 21:00:28 +0200

    TicTacToe 1-st stage

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,11 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args){
+        FieldPrint();
+    }
+
+    private static void FieldPrint(){
+        System.out.println("XOX\nOXO\nOXO");
+    }
+}
