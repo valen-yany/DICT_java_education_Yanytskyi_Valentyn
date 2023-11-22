9978c666bac521a7eae52480b6c63a472e408f19
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-11-22 21:40:52 +0200

    TicTacToe 2-nd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,11 +1,42 @@
 package TicTacToe;
+import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args){
-        FieldPrint();
+        Board b = new Board();
+        b.PrintGrid();
     }
 
-    private static void FieldPrint(){
-        System.out.println("XOX\nOXO\nOXO");
+
+}
+
+class Board{
+    private final char[][] grid = new char[3][3];
+    public Board(){
+        String field = "";
+        while (field.length() != 9 || !field.matches(".*[XO_]")){
+            System.out.print("Enter cells: ");
+            Scanner in = new Scanner(System.in);
+            field = in.next();
+        }
+        SetGrid(field.toCharArray());
+    }
+
+    private void SetGrid(char[] field){
+        for(int i=0;i<9;i++){
+            grid[i/3][i%3] = field[i];
+        }
+    }
+    
+    public void PrintGrid(){
+        System.out.println("-----");
+        for (char[] line:grid) {
+            System.out.print("|");
+            for(char cell:line){
+                System.out.print(cell);
+            }
+            System.out.println("|");
+        }
+        System.out.println("-----");
     }
 }
